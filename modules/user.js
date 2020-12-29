7216dbbbd39c8791e1a1092ee1f040c84a386161
26-Anuj <59211195+Amitk6228@users.noreply.github.com>
2020-12-30 03:01:44 +0530

    final commit

---
+++ modules/user.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose')
+// mongoose.connect('mongodb://localhost:27017/pms',{useNewUrlParser:true,useCreateIndex:true,useUnifiedTopology:true})
+mongoose.connect('mongodb+srv://Amit_Kumar:Amitk6228@cluster0.vuxfq.mongodb.net/DrBot?retryWrites=true&w=majority',{useNewUrlParser:true,useCreateIndex:true,useUnifiedTopology:true})
+var conn=mongoose.Collection
+var userScheme= new mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        index:{
+            unique:true,
+        }
+    },
+    phone:{
+      type:String,
+      required:true,
+      index:{
+          unique:true,
+      }
+    },
+    email:{
+        type:String,
+        required:true,
+        index:{
+            unique:true,
+        }
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+    date:{
+        type:Date,
+        default:Date.now
+    }
+})
+var userModel = mongoose.model('users',userScheme)
+module.exports=userModel
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,95 @@
 var express = require('express');
 var router = express.Router();
+var userModule = require('../modules/user');
+var jwt = require('jsonwebtoken')
+if (typeof localStorage === "undefined" || localStorage === null) { //npm- localStorage requirement 
+  var LocalStorage = require('node-localstorage').LocalStorage;
+  localStorage = new LocalStorage('./scratch');
+}
 
+var loginUser = localStorage.getItem('loginUser')
+
+function verifyusername(req,res,next){
+  var uname  = req.body.uname
+  var checkUsername = userModule.findOne({username:uname})
+  checkUsername.exec((err,data)=>{
+    if(err)throw(err)
+    if(!data){
+      return  res.render('index', { title: 'Password Management System',msg:"Invalid Username !!",success:''}); //this will print error on html page otherthan console
+      }
+    next()
+  })
+}
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var loginUser = localStorage.getItem('loginUser')
+  console.log("loginuser:",loginUser)
+  res.render('index', { title: 'DrBot | AI Health Assistant',loginUser:loginUser });
 });
 
 router.get('/covid', function(req, res, next) {
-  res.render('covid');
+  res.render('covid', { title: 'DrBot | AI Health Assistant',loginUser:loginUser });
+});
+router.get('/diabetes', function(req, res, next) {
+  res.render('diabetes', { title: 'DrBot | AI Health Assistant',loginUser:loginUser });
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'DrBot | Login',msg:"" ,success:'',loginUser:loginUser});
+});
+
+router.post('/login', verifyusername,function(req, res, next) {
+  var username = req.body.uname //importing username from html form
+  var password = req.body.password //importing password from html form
+  var checkUser = userModule.findOne({username:username}) //this function will find that username in db 
+  checkUser.exec((err,data)=>{
+  if (err) throw err
+  var getUserID= data._id //this will fetch the id of the user from db
+  var getPassword=data.password //and this will store the input password
+  if(password==getPassword){ //and this will compare it for further login
+    var token = jwt.sign({ userID: getUserID }, 'loginToken'); //this will get store getUserID into userID from the db and create a token for that user named a 'loginToken'
+    localStorage.setItem('userToken', token); // and this will store that token temporary in scratch folder named as 'userToken'
+    localStorage.setItem('loginUser', username); //and store the username into 'loginuser' and help in displaying the name of the user on further pages
+    res.redirect('/')
+    // res.render('index', { title: 'Password Management System',msg:"Logged in Sucessfully" });
+  }else{
+    res.render('login', { title: 'DrBot | Login ',msg:"Invalid Username and Password" ,success:''});
+  }
+  })
+});
+
+router.get('/register', function(req, res, next) {
+  res.render('register',{ title: 'DrBot | Register',msg:'',success:'' });
+});
+router.post('/register',function(req, res, next) {  //note:- we are using these middlewere here so that our error will not be printed in console and server will not be stopped it will be displayed in form
+  var username = req.body.uname //importing userDetails from html form
+  var email = req.body.email
+  var phone = req.body.phone 
+  var password = req.body.password 
+  var confpassword= req.body.confpassword
+
+  if(password != confpassword){
+    res.render('register', { title: 'Password Management System',msg:"Password not matched",success:'',loginUser:loginUser});
+  }
+  else{
+  var userDetails = new userModule({ //exporting it to MongoDB model 
+    username:username,
+    email:email,
+    phone:phone,
+    password:password
+  })
+  userDetails.save((err,doc)=>{
+    if(err)throw err
+    res.render('register', { title: 'Password Management System',success:"User Registered Successfully",msg:'',loginUser:loginUser});
+  }) //now last step, saving it 
+}
+});
+
+//-----Logout----
+router.get('/logout', function(req, res, next) {
+  localStorage.removeItem('userToken')
+  localStorage.removeItem('loginUser')
+  res.redirect('/')
 });
 
 module.exports = router;
